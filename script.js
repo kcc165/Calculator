4c13f87a88fe60bddd4184a15a69c6812300e3aa
kcc165 <kcc165@msstate.edu>
2019-09-30 12:08:04 -0500

    Add operation functions and button functionality

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-const textArea = document.querySelector("#text-area");
+const screenArea = document.querySelector("#screen-area");
 const numbers = document.querySelectorAll(".number-button");
 console.log(numbers);
 
 numbers.forEach((num) => {
     num.addEventListener("click", (e) => {
-        console.log(e);
+        let textnode = document.createTextNode(e.target.firstChild.data)
+        screenArea.appendChild(textnode);
+        console.log(e.target.firstChild.data);
     });
 } );
+
+
+
+
+function add (num1, num2){
+    return num1 + num2;
+}
+
+function subtract (num1, num2){
+    return num1 - num2;
+}
+
+function multiply (num1, num2){
+    return num1 * num2;
+}
+
+function divide (num1, num2){
+    return num1 / num2;
+}
+
+console.log(multiply(5,3));
