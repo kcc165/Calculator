9ff5834707082b64eed9331ff03daa98f55e6112
kcc165 <kcc165@msstate.edu>
2019-09-30 09:54:31 -0500

    Add number button listen event

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const textArea = document.querySelector("#text-area");
 const numbers = document.querySelectorAll(".number-button");
 console.log(numbers);
+
+numbers.forEach((num) => {
+    num.addEventListener("click", (e) => {
+        console.log(e);
+    });
+} );
