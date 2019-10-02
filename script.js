fa914eacb40a40e2c0d8ef0cccf3cc4fef3abf27
kcc165 <kcc165@msstate.edu>
2019-10-02 12:32:04 -0500

    Functionality for displaying numbers with operator

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const screenArea = document.querySelector("#screen-area");
 const numbers = document.querySelectorAll(".number-button");
 const operations = document.querySelectorAll(".operation-button");
 let textnode = document.createTextNode("");
-let number1 = "";
-let number2 = "";
+let number1 = [];
+let number2 = [];
 let operator = "";
 
 
@@ -11,9 +11,27 @@ numbers.forEach((num) => {
     num.addEventListener("click", (e) => {
         textnode.nodeValue += e.target.firstChild.data
         screenArea.appendChild(textnode);
-        console.log(e.target.firstChild.data);
+        if (operator === ""){
+            number1.push(e.target.firstChild.data);
+        }
+        else {
+            number2.push(e.target.firstChild.data);
+        }
+        console.log(number1);
+        console.log(number2);
     });
 } );
+
+operations.forEach((oper) => {
+    oper.addEventListener("click", (e) => {
+        if (number1 !== ""){
+            textnode.nodeValue += ` ${e.target.firstChild.data} `;
+            operator = e.target.id;
+            
+            
+        }
+    });
+});
 
 
 
@@ -34,4 +52,21 @@ function divide (num1, num2){
     return num1 / num2;
 }
 
-console.log(multiply(5,3));
+function equals (num1, num2, operator){
+    if (operator === "plus"){
+       return add(num1, num2);
+    }
+
+    else if (operator === "subtract"){
+        return subtract(num1, num2);
+    }
+
+    else if (operator === "multiply"){
+        return multiply(num1, num2);
+    }
+
+    else if (operator === "divide"){
+        return divide(num1, num2);
+    }
+}
+
